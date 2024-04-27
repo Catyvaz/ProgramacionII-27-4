248045d55fea0b641a4dccd41c833977e5f0d5cf
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-04-27 19:09:11 -0300

    Agrego comentarios y comienzo con la consigna 1

---
+++ cons1.py
@@ -0,0 +1,10 @@
+#Escribe un programa que solicite al usuario que ingrese su peso en kilogramos y su altura en metros, luego calcule y muestre su IMC.
+#Instrucciones:
+#Solicita al usuario que ingrese su peso en kilogramos.
+#Solicita al usuario que ingrese su altura en metros.
+#Calcula el IMC utilizando la fórmula proporcionada.
+#Muestra el resultado al usuario.
+
+print(float(input("Ingrese su peso en kilogramos: ")))
+print(float(input("Ingrese su altura en metros: ")))
+ 
