5fc0f15fe8fb64c65a5866a1895209192e81a203
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-04-27 23:11:40 -0300

    ejercicio 3 terminado

---
+++ ejercicio3.py
@@ -0,0 +1,13 @@
+#Área de un Triángulo
+#Escribe un programa en Python que solicite al usuario que ingrese la base y la altura de un triángulo,
+#.y luego calcule y muestre el área del triángulo.
+
+base = float(input("Ingrese el tamaño de la base: "))
+altura = float(input("Ingrese el tamaño de la altura: "))
+
+areaT = (base * altura)/2
+
+print("El area del triangulo de base {} y altura {} es: {}".format(base, altura, areaT))
+
+
+
