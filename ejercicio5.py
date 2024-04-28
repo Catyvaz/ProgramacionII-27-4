60aab7f64706f5f6f292b4a87eea0064fb355ccd
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-04-28 18:45:17 -0300

    ejercicio 5 terminado

---
+++ ejercicio5.py
@@ -0,0 +1,16 @@
+#Conversor de Temperatura (Actualización)
+# Mejora el programa de conversión de temperatura que escribiste anteriormente
+# para que permita al usuario elegir entre convertir de grados Celsius a grados Fahrenheit o viceversa.
+
+print("Seleccione que tipo de conversion desea.\n", "Escriba Cel si desea pasar de Fahrenheit a Celsius.\n", "Escriba Far si desea pasar de Celsius a Fahrenheit.\n")
+eleccion = (input("Escriba su selección: ")).lower()
+
+if eleccion == "cel":
+    temperatura = float(input("Coloque la temperatura en Fahrenheit: "))
+    resultado = (temperatura - 32)/1.8
+
+elif eleccion == "far":
+    temperatura = float(input("Coloque la temperatura en Celsius: "))
+    resultado = (temperatura * 1.8) + 32
+
+print("El resultado de la conversion es: {}".format(resultado))
