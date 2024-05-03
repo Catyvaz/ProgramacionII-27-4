15c40a87265eee2393772fcc17bf3a42e8a2c857
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-05-03 00:37:34 -0300

    ejercicio 7

---
+++ ejercicio7.py
@@ -0,0 +1,7 @@
+#Juego de Adivinar el Número
+# Desarrolla un juego en el que el programa selecciona aleatoriamente un número entre 1 y 100 y el jugador debe adivinarlo. 
+# El programa debe proporcionar pistas al jugador si el número ingresado es demasiado alto o demasiado bajo. 
+# El juego debe continuar hasta que el jugador adivine correctamente el número.
+
+numero = random(1,101)
+print(numero)
