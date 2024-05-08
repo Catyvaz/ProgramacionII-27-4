2811737c30e6b79e450b6e240fbba3b62341eb0b
Caty <catybusvaz2@gmail.com>
2024-05-08 13:20:21 -0300

    Ejercicio 7 terminado

--- ejercicio7.py
+++ ejercicio7.py
@@ -2,6 +2,16 @@
 # Desarrolla un juego en el que el programa selecciona aleatoriamente un número entre 1 y 100 y el jugador debe adivinarlo. 
 # El programa debe proporcionar pistas al jugador si el número ingresado es demasiado alto o demasiado bajo. 
 # El juego debe continuar hasta que el jugador adivine correctamente el número.
+import random
 
-numero = random(1,101)
-print(numero)
+numero = random.randint(1,100)
+
+while True:
+    respuesta = int(input("Que número será?: "))
+    if respuesta < numero:
+        print("El número es más grande.")
+    elif respuesta > numero:
+        print("El número es más chico")
+    elif respuesta == numero:
+        print(f"¡Adivinaste el número!, era el {numero}")
+        break
