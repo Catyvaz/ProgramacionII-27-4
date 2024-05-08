e2bca4770e5be091936c6a52392558f68c306d80
Caty <catybusvaz2@gmail.com>
2024-05-08 12:55:28 -0300

    Ejercicio 6 terminado

--- ejercicio6.py
+++ ejercicio6.py
@@ -6,14 +6,45 @@
 
 contraseña = input("Ingrese su contraseña. \n*Recuerde que debe contener, al menos; \n - Una mayúscula.\n - Una minuscula.\n - Un número.\n - Un caracter especial (!, @, #, $, %, etc)* \nContraseña:")
 
-def contiene_(lista):
-    esta = False
+def mayuscula(lista):
+    estado = False
     for i in range(len(lista)):
         value = lista[i]
+        if lista[i].isupper():
+            estado = True
+            break
+    return estado
 
-if len(contraseña > 7):
-    value = #función
-    if value :
+def minuscula(lista):
+    estado = False
+    for i in range(len(lista)):
+        value = lista[i]
+        if lista[i].islower():
+            estado = True
+            break
+    return estado
+
+def numeros(lista):
+    estado = False
+    for i in range(len(lista)):
+        value = lista[i]
+        if lista[i].isdigit():
+            estado = True
+            break
+    return estado
+
+def caracter(lista):
+    estado = False
+    for i in range(len(lista)):
+        value = lista[i]
+        if not(lista[i].isalpha() or lista[i].isdigit()):
+            estado = True
+            break
+    return estado
+
+if len(contraseña) > 7:
+    value = mayuscula(contraseña) and minuscula(contraseña) and numeros(contraseña) and caracter(contraseña)
+    if value:
         print("Su contraseña es válida")
     else:
         print("Contraseña no válida")
