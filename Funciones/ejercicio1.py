c88c62a61b6b5a60bbee3e4d30f3f8e4ee7913a1
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-05-15 00:56:20 -0300

    ejercicio 1 casi terminado

--- Funciones/ejercicio1.py
+++ Funciones/ejercicio1.py
@@ -3,3 +3,17 @@
 # cadena con un espacio adicional tras cada letra. Por ejemplo, “Hola, tú” devolverá “H o l a , t ú “. 
 # Crea un programa principal donde se use dicha función luego del ingreso del usuario.
 
+def ConvertirEspaciado(lista):
+    nueva = []
+    contador = 0
+    for i in range (len(lista)):
+        if lista[i] == " ":
+            nueva.append(lista[i])
+        else:
+            nueva.append(lista[i] + " ")
+    return "".join(nueva)
+
+oracion = input("Ingrese su oración: ")
+convercion = ConvertirEspaciado(oracion)
+print(convercion)
+
